95fb672af81f805b3ca36ab6e3f34770668c4f60
Tomver27 <tomasv0127@gmail.com>
2025-05-10 11:03:44 -0500

    Add new elements in SideBar

--- src/components/SidebarSocial.jsx
+++ src/components/SidebarSocial.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
-import { FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
+import {
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaBriefcase
+} from 'react-icons/fa';
 import './SidebarSocial.css';
 
 export default function SidebarSocial() {
   return (
     <div className="sidebar-social">
-      <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-      <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-      <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+      <a
+        href="https://github.com/Tomver27"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub />
+      </a>
+      <a
+        href="https://twitter.com/Tomver27"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTwitter />
+      </a>
+      <a
+        href="https://instagram.com/Tomver27"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram />
+      </a>
+      {/* Elempleo */}
+      <a
+        href="https://www.elempleo.com/perfil/Tomver27"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaBriefcase />
+      </a>
+      {/* LinkedIn */}
+      <a
+        href="https://www.linkedin.com/in/Tomver27"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin />
+      </a>
     </div>
   );
 }
